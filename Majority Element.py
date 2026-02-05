776b39a3486c5a17e8b8b7f4a6264d13ee76676b
You55efHany <hanyyousif115@gmail.com>
2026-02-05 20:06:20 +0200

    Create Majority Element.py

---
+++ Majority Element.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        for k,v in c.items():
+            if (v > len(nums) / 2):
+                return k
+        
