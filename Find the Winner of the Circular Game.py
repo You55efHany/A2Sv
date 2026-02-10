ef56698be7372af2613bcadf51d20684d8b67080
You55efHany <hanyyousif115@gmail.com>
2026-02-10 21:19:50 +0200

    Create Find the Winner of the Circular Game.py

---
+++ Find the Winner of the Circular Game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        lst = list(range(1, n + 1))
+        idx = 0
+        while len(lst) > 1:
+            idx = (idx + k - 1) % len(lst)
+            lst.pop(idx)
+        return lst[0]
